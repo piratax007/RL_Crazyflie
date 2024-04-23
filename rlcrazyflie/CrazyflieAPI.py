63ef96d4091f70e42c41395cbd1cb6bf76b96491
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-04-23 10:43:30 +0200

    Improved naming and typos

--- rlcrazyflie/CrazyflieAPI.py
+++ rlcrazyflie/CrazyflieAPI.py
@@ -1,6 +1,6 @@
 from crazyflie_py import Crazyswarm
 import numpy as np
-from control import _RLControlForCrazyflie
+from control import RLControlForCrazyflie
 
 
 class _CrazyflieAPI:
@@ -10,8 +10,8 @@ class _CrazyflieAPI:
         self.CRAZYFLIE_MASS = 0.027
         self.CRAZYFLIE_KF = 3.16e-10
         self.HOVER_RPM = np.sqrt((self.GRAVITY*self.CRAZYFLIE_MASS)/(4*self.CRAZYFLIE_KF))
-        self.state = np.zeros((1, 12))
-        self.control = _RLControlForCrazyflie(hover_rpm=self.HOVER_RPM, state=self.state)
+        self.INITIAL_STATE = np.zeros((1, 12))
+        self.control = RLControlForCrazyflie(hover_rpm=self.HOVER_RPM, initial_state=self.INITIAL_STATE)
 
     @staticmethod
     def _get_drone(crazyflie_id: int):
@@ -19,4 +19,4 @@ class _CrazyflieAPI:
         return swarm.dron.drones[crazyflie_id]
 
 
-drone = _CrazyflieAPI()
+crazyflie = _CrazyflieAPI()
