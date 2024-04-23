ceddb48a0ba4ad832513212f794ad7028310d05f
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-04-23 09:29:26 +0200

    Developed a new class with all the RL stuff to use the RL policy and return the PWM

--- rlcrazyflie/control.py
+++ rlcrazyflie/control.py
@@ -1,44 +1,35 @@
-from crazyflie_py import Crazyswarm
 import numpy as np
 import os
 from stable_baselines3 import PPO
 
 
-def get_policy(policy_path):
-    if os.path.isfile(policy_path + '/best_model.zip'):
-        return PPO.load(policy_path + '/best_model.zip')
+class _RLControlForCrazyflie:
+    def __init__(
+            self,
+            policy_path=os.path.dirname('../assets'),
+            hover_rpm=14468.42,
+            state=np.zeros((1, 12))
+    ):
+        self.policy = self._get_policy(policy_path)
+        self.observation_space = state
+        self.reference_rpm = hover_rpm
+        self.calculated_rpm = np.zeros((1, 4))
 
-    raise Exception("[ERROR]: no model under the specified path", policy_path)
+    def denormalize_actions(self):
+        normalize_actions, _state = self.policy.predict(
+            self.observation_space,
+            deterministic=True,
+        )
 
+        self.calculated_rpm[0, :] = np.array(self.reference_rpm*(1+0.05*normalize_actions[0, :]))
 
-def main(policy_path):
-    swarm = Crazyswarm()
-    cf = swarm.allcfs.crazyflies[0]
-    policy = get_policy(policy_path)
+    def applicable_pwm(self):
+        self.denormalize_actions()
+        return tuple(map(lambda rpm: (rpm-4070.3)/0.2685, self.calculated_rpm))
 
-    # Call to getObservations (VICON)
-    obs = np.zeros((1, 12))
+    @staticmethod
+    def _get_policy(policy_path: str) -> PPO.policy:
+        if os.path.isfile(policy_path + '/best_model.zip'):
+            return PPO.load(policy_path + '/best_model.zip')
 
-    # Here a need a loop?
-    action, _states = policy.predict(obs,
-                                     deterministic=True
-                                     )
-
-    # convert action into PWM
-
-    cf.setParam("motorPowerSet.enable", 1)
-    cf.setParam("motorPowerSet.m1", action[0][0])
-    cf.setParam("motorPowerSet.m2", action[0][1])
-    cf.setParam("motorPowerSet.m3", action[0][2])
-    cf.setParam("motorPowerSet.m4", action[0][3])
-    cf.setParam("motorPowerSet.enable", 0)
-
-    # How can I land the drone after the mission ends?
-
-
-if __name__ == '__main__':
-    POLICY_PATH = os.path.dirname(
-        '../assets'
-    )
-
-    main(POLICY_PATH)
+        raise Exception("[ERROR]: no model under the specified path", policy_path)
