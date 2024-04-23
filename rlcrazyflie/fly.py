b130891a5bc11cd364bb49842d3c89912ffa0b92
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-04-23 10:44:19 +0200

    Improved naming using the correct attribute

--- rlcrazyflie/fly.py
+++ rlcrazyflie/fly.py
@@ -1,14 +1,19 @@
-from CrazyflieAPI import drone
+from CrazyflieAPI import crazyflie
 
 
 def main():
-    actions = drone.control.applicable_pwm()
-    drone.setParam("motorPowerSet.enable", 1)
-    drone.setParam("motorPowerSet.m1", actions[0])
-    drone.setParam("motorPowerSet.m2", actions[1])
-    drone.setParam("motorPowerSet.m3", actions[2])
-    drone.setParam("motorPowerSet.m4", actions[3])
-    drone.setParam("motorPowerSet.enable", 0)
+    crazyflie.drone.setParam("motorPowerSet.enable", 1)
+
+    actions = crazyflie.control.applicable_pwm()
+
+    crazyflie.drone.setParam("motorPowerSet.m1", actions[0])
+    crazyflie.drone.setParam("motorPowerSet.m2", actions[1])
+    crazyflie.drone.setParam("motorPowerSet.m3", actions[2])
+    crazyflie.drone.setParam("motorPowerSet.m4", actions[3])
+
+    crazyflie.drone.land(targetHeight=0.04, duration=2.5)
+
+    crazyflie.drone.setParam("motorPowerSet.enable", 0)
 
 
 if __name__ == '__main__':
