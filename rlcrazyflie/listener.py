c5a00ec0a3295d6a8452d036546d310e830a8978
Fausto M. Lagos S <piratax007@protonmail.ch>
2024-05-02 12:32:45 +0200

    Added topic start engines to control the turn on and correct the position of the drone in the arena, and added the stop engines topic to turn off the propellers

--- rlcrazyflie/listener.py
+++ rlcrazyflie/listener.py
@@ -1,34 +1,35 @@
 import numpy as np
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import TransformStamped
-from tf2_msgs.msg import TFMessage
 from nav_msgs.msg import Odometry
+from std_msgs.msg import String
 from rlcrazyflie.CrazyflieAPI import crazyflie
 
-TOPIC1 = "/tf"
-TOPIC2 = "/vicon/fausto_crazyfly/fausto_crazyfly"
-TOPIC3 = "/vicon/fausto_crazyfly/fausto_crazyfly/odom"
+TOPIC_ODOMETRY = "/vicon/fausto_crazyfly/fausto_crazyfly/odom"
+TOPIC_START_ENGINES = "/startfly"
+TOPIC_STOP_ENGINES = "/stopfly"
 
 
 class MySubscriber(Node):
     def __init__(self):
         super().__init__('listener')
         self.subscription1 = self.create_subscription(
-            TFMessage,
-            TOPIC1,  # Change 'topic1' to the name of the first topic you want to subscribe to
+            Odometry,
+            TOPIC_ODOMETRY,  # Change 'topic3' to the name of the third topic you want to subscribe to
             self.listener_callback,
             10)
         self.subscription2 = self.create_subscription(
-            TransformStamped,
-            TOPIC2,  # Change 'topic2' to the name of the second topic you want to subscribe to
-            self.listener_callback,
-            10)
+            String,
+            TOPIC_START_ENGINES,
+            self.listener_startcv,
+            1
+        )
         self.subscription3 = self.create_subscription(
-            Odometry,
-            TOPIC3,  # Change 'topic3' to the name of the third topic you want to subscribe to
-            self.listener_callback,
-            10)
+            String,
+            TOPIC_STOP_ENGINES,
+            self.listener_stopcv,
+            1
+        )
 
         self.tf = None
         self.tf_stamped = None  # position
@@ -38,75 +39,62 @@ class MySubscriber(Node):
         self.linear_velocities = ()
         self.angular_velocities = ()
         self.crazyflie = crazyflie
+        self.start_ref = (0, 0, 0)
+        self.start = 0
 
     def listener_callback(self, msg):
-        if isinstance(msg, TFMessage):
-            # Handle TFMessage
-            # for transform in msg.transforms:
-                # Access transform data as needed
-                # child_frame_id = transform.child_frame_id
-                # transform_matrix = transform.transform
-                # Print transform data for demonstration
-                # print("Received TF Message:")
-                # print("Child Frame ID:", child_frame_id)
-                # print("Transform Matrix:")
-                # print("  Translation: [x={}, y={}, z={}]".format(
-                #     transform_matrix.translation.x,
-                #     transform_matrix.translation.y,
-                #     transform_matrix.translation.z))
-                # print("  Rotation: [x={}, y={}, z={}, w={}]".format(
-                #     transform_matrix.rotation.x,
-                #     transform_matrix.rotation.y,
-                #     transform_matrix.rotation.z,
-                #     transform_matrix.rotation.w))
-                
-            self.tf = msg
-                
-        elif isinstance(msg, TransformStamped):
-            # Handle TransformStamped
-            # translation = msg.transform.translation
-            # rotation = msg.transform.rotation
-            # Print transform data for demonstration
-            # print("Received TransformStamped message:")
-            # print("Translation: [x={}, y={}, z={}]".format(
-            #     translation.x, translation.y, translation.z))
-            # print("Rotation: [x={}, y={}, z={}, w={}]".format(
-            #     rotation.x, rotation.y, rotation.z, rotation.w))
-            
-            self.tf_stamped = msg
-            
-        elif isinstance(msg, Odometry):
-            self.position = (msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z)
-            quaternion = msg.pose.pose.orientation
-            self.euler_angles = tuple(quaternion_to_euler_angles(quaternion))
-            self.linear_velocities = (msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z)
-            self.angular_velocities = (msg.twist.twist.angular.x, msg.twist.twist.angular.y, msg.twist.twist.angular.z)
+        self.position = (
+            msg.pose.pose.position.x - self.start_ref[0],
+            msg.pose.pose.position.y - self.start_ref[1],
+            msg.pose.pose.position.z - self.start_ref[2]
+        )
+        quaternion = (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
+        self.euler_angles = quaternion_to_euler_angles(quaternion)
+        self.linear_velocities = (msg.twist.twist.linear.x, msg.twist.twist.linear.y, msg.twist.twist.linear.z)
+        self.angular_velocities = (msg.twist.twist.angular.x, msg.twist.twist.angular.y, msg.twist.twist.angular.z)
 
-            print(f"""
-            ####### ODOMETRY INFORMATION ##########
-            POSITION {self.position}
-            ---------------------------------------
-            QUATERNION {quaternion}
-            ROLL, PITCH, YAW = {self.euler_angles}
-            ---------------------------------------
-            VELOCITY {self.linear_velocities}
-            ANGULAR VELOCITY {self.angular_velocities}
-            """)
-
-            self.odom = msg
-            
-        if self.tf is not None and self.odom is not None and self.tf_stamped is not None:
-            self.crazyflie.control.observation_space = np.hstack(
-                (
-                    self.position,
-                    self.euler_angles,
-                    self.linear_velocities,
-                    self.angular_velocities
-                )
+        self.crazyflie.control.observation_space = np.array([np.hstack(
+            (
+                self.position,
+                self.euler_angles,
+                self.linear_velocities,
+                self.angular_velocities
             )
+        )])
+        if self.start == 1:
+            actions = self.crazyflie.control.applicable_pwm()
+            # print(f"""
+            #         Observarions: {crazyflie.control.observation_space}
+            #         PWM Control: {actions}
+            #         """)
+            print(f"""
+                    PWM Control: {actions}
+                    """)
+            fly(actions)
+
+    def listener_startcv(self, msg):
+        if self.start == 0:
+            ref_x = self.position[0]
+            ref_y = self.position[1]
+            ref_z = self.position[2]
+            self.start_ref = (ref_x, ref_y, ref_z)
+            self.start = 1
+
+    def listener_stopcv(self, msg):
+        if self.start == 1:
+            self.start = 0
+            fly((0, 0, 0, 0))
+            crazyflie.drone.setParam("motorPowerSet.enable", 0)
 
 
-def quaternion_to_euler_angles(q: list):
+def fly(actions: tuple):
+    crazyflie.drone.setParam("motorPowerSet.m1", actions[0])
+    crazyflie.drone.setParam("motorPowerSet.m2", actions[1])
+    crazyflie.drone.setParam("motorPowerSet.m3", actions[2])
+    crazyflie.drone.setParam("motorPowerSet.m4", actions[3])
+
+
+def quaternion_to_euler_angles(q: tuple):
     x, y, z, w = q
 
     sinr_cosp = 2 * (w * x + y * z)
@@ -127,6 +115,7 @@ def quaternion_to_euler_angles(q: list):
 
 
 def main():
+    crazyflie.drone.setParam("motorPowerSet.enable", 1)
     my_subscriber = MySubscriber()
     rclpy.spin(my_subscriber)
     my_subscriber.destroy_node()
